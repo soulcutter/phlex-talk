144f7dec3e87031c4d156898c5250da43111ad97
Bradley Schaefer <bradley.schaefer@gmail.com>
2023-08-16 11:10:37 -0400

    testing/environment housekeeping

--- spec/example_phlex_spec.rb
+++ spec/example_phlex_spec.rb
@@ -3,15 +3,13 @@ require "phlex-testing-nokogiri"
 RSpec.describe ExamplePhlex do
   include Phlex::Testing::Nokogiri::DocumentHelper
 
-  def template_params(**kwargs)
-    {
+  def template_params(**kwargs) = {
       name: "Bradley Schaefer", 
       code: "Soulcutter",
       desc: "An okay human", 
       features: %w[wingding thing-a-magig doodad],
       cost: 11,
-    }.merge(kwargs.slice(:name, :code, :desc, :features, :cost))
-  end
+  }.merge(kwargs.slice(:name, :code, :desc, :features, :cost))
 
   it "renders a list" do
     output = render ExamplePhlex.new(**template_params(features: %w[one two]))
